b5bc3899165a68a5299716491c30136cc4993f12
merkylove <merkylove@users.noreply.github.com>
2018-10-30 10:47:41 +0300

    Update install_pyspark.sh

--- install_pyspark.sh
+++ install_pyspark.sh
@@ -2,4 +2,4 @@
 wget http://ftp.byfly.by/pub/apache.org/spark/spark-2.3.2/spark-2.3.2-bin-hadoop2.7.tgz  &&
 tar -xzf spark-2.3.2-bin-hadoop2.7.tgz &&
 mv spark-2.3.2-bin-hadoop2.7 /opt/spark-2.3.2 &&
-ln -s /opt/spark-1.2.0 /opt/spark̀
+ln -s /opt/spark-2.3.2 /opt/spark̀
