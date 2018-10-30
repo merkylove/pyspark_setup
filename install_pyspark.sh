3be623d1c82a4b73efcda7557e8f5495dddde9e5
merkylove <merkylove@users.noreply.github.com>
2018-10-30 10:46:46 +0300

    fixed url

--- install_pyspark.sh
+++ install_pyspark.sh
@@ -1,5 +1,5 @@
 # replace version of spark with the one from http://spark.apache.org/downloads.html
-wget https://www.apache.org/dyn/closer.lua/spark/spark-2.3.2/spark-2.3.2-bin-hadoop2.7.tgz &&
+wget http://ftp.byfly.by/pub/apache.org/spark/spark-2.3.2/spark-2.3.2-bin-hadoop2.7.tgz  &&
 tar -xzf spark-2.3.2-bin-hadoop2.7.tgz &&
 mv spark-2.3.2-bin-hadoop2.7 /opt/spark-2.3.2 &&
 ln -s /opt/spark-1.2.0 /opt/spark̀
